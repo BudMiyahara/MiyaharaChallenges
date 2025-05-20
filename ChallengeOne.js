8ce2e219575beb8d14f01b260577fad2ea82c0a1
Miyahara <25MiyaharaB@badinhs.org>
2025-05-19 22:07:23 -0400

    finished challenge

---
+++ ChallengeOne.js
@@ -0,0 +1,17 @@
+// problem 4
+
+let number = 600851475143
+
+let factor = 2
+
+while(factor * factor <= number) {
+    if (number % factor == 0) {
+        number = number / factor;
+
+    } else{factor ++};
+    console.log(factor)
+running = false;
+
+}
+
+
